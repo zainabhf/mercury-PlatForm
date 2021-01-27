141e506210cb9a4e326ae6157eb980866691d44b
ghosoonfk <ghosooonf@gmail.com>
2021-01-27 15:05:50 +0300

    Dao

--- src/main/java/com/ga/mercury/controller/CourseController.java
+++ src/main/java/com/ga/mercury/controller/CourseController.java
@@ -1,8 +1,25 @@
 package com.ga.mercury.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CourseController {
 
+	
+	@Autowired 
+	private Environment env;
+	@GetMapping("course/add")
+	public ModelAndView addCourse(){
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("course/add");
+		HomeController hc = new HomeController();
+		hc.setAppName(mv, env);
+		return mv;
+		
+	}
+	
 }
