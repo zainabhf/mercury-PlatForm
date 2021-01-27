c56336f61ce55e94f772f4813c124de00ed6a85f
ghosoonfk <ghosooonf@gmail.com>
2021-01-27 15:37:41 +0300

    adding to the layout

--- src/main/java/com/ga/mercury/controller/CourseController.java
+++ src/main/java/com/ga/mercury/controller/CourseController.java
@@ -6,20 +6,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.ga.mercury.dao.TeacherDao;
+
 @Controller
 public class CourseController {
 
+	@Autowired
+	private TeacherDao teacher;
 	
 	@Autowired 
 	private Environment env;
+	
+	
 	@GetMapping("course/add")
 	public ModelAndView addCourse(){
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("course/add");
 		HomeController hc = new HomeController();
 		hc.setAppName(mv, env);
+		var te = teacher.findAll();
+		mv.addObject("teachers", te);
 		return mv;
 		
 	}
+	
+	
+	
 	
 }
--- src/main/webapp/views/shared/_layout.jsp
+++ src/main/webapp/views/shared/_layout.jsp
@@ -7,6 +7,7 @@
 <title>Insert title here</title>
 </head>
 <body>
-<h1>working</h1>
+
+<h1>${Welcome}</h1>
 </body>
 </html>
