ff19b7e6248b73f727555539ab6e5283e607a87d
moudikhalid96 <moudi_khalid96@hotmail.com>
2021-01-27 10:07:12 +0300

    add user class

---
+++ src/main/java/com/ga/mercury/model/User.java
@@ -0,0 +1,59 @@
+package com.ga.mercury.model;
+
+import java.util.Set;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	private String fullName; 
+	private String emailAddress; 
+	private String password; 
+	private String gender; 
+	private String role;
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+}
